fb2a3c8d0b71e505457b21191badb15019dd1148
matmil92@outlook.com <matmil92@outlook.com>
2017-03-17 19:47:17 +0100

    nonCommitClassAdded

---
+++ testWebApi/testWebApi/Models/TestNonCommit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testWebApi.Models
+{
+    public class TestNonCommit
+    {
+        private String javaStyleName;
+        private Int32 javaStyleNumber;
+
+        public String getJavaStyleName()
+        {
+            return javaStyleName;
+        }
+        public Int32 getJavaStylNumber()
+        {
+            return javaStyleNumber;
+        }
+        public void setJavaStyleName(String javaStyleName)
+        {
+            this.javaStyleName = javaStyleName;
+        }
+        public void getJavaStylNumber(Int32 javaStyleNumber)
+        {
+            this.javaStyleNumber = javaStyleNumber;
+        }
+    }
+}
